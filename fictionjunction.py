5667a2794a2c43425a6c7416a144fe1dce921ee2
Gea-Suan Lin <gslin@gslin.org>
2018-02-25 17:10:41 +0800

    Add skeleton.

---
+++ fictionjunction.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+class FictionJunction(object):
+    def __init__(self):
+        pass
+
+    def start(self):
+        pass
+
+if '__main__' == __name__:
+    fj = FictionJunction()
+    fj.start()
