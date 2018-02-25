18598f688bfd1f95dc275c0bacf40f2b5d400193
Gea-Suan Lin <gslin@gslin.org>
2018-02-25 17:29:49 +0800

    Get login page.

--- fictionjunction.py
+++ fictionjunction.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python3
 
+import requests
+
 class FictionJunction(object):
+    __url_login = 'http://www.fictionjunction.org/logging.php?action=login'
+
     def __init__(self):
         pass
 
+    def login(self):
+        res = self.r.get(self.__url_login)
+
     def start(self):
-        pass
+        self.login()
 
 if '__main__' == __name__:
     fj = FictionJunction()
