5dd2c9bb3d19f25f1a9f133e4ba73baf49e4c60f
Gea-Suan Lin <gslin@gslin.org>
2018-02-26 21:26:48 +0800

    Skeleton.

---
+++ fictionjunction-privmsg.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+class FictionJunctionPrivmsg(object):
+    def __init__(self):
+        pass
+
+    def start(self):
+        pass
+
+if '__main__' == __name__:
+    fj = FictionJunctionPrivmsg()
+    fj.start()
