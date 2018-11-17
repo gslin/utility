a00cc844bbc595834cd20a0027e42d1bfe54b112
Gea-Suan Lin <gslin@gslin.org>
2018-11-18 07:43:09 +0800

    Read configuration from ~/.config/twitter-list-members-move/config.ini.

--- twitter-list-members-move.py
+++ twitter-list-members-move.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3
 
+import configparser
+import os
+
 def work():
-    pass
+    home = os.environ['HOME']
+    f_conf = '{}/.config/twitter-list-members-move/config.ini'.format(home)
+
+    c = configparser.ConfigParser()
+    c.read(f_conf)
 
 if '__main__' == __name__:
     work()
