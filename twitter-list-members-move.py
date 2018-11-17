994a4dd4882fa8f800bdb9df4551da27481412ba
Gea-Suan Lin <gslin@gslin.org>
2018-11-18 07:45:17 +0800

    Use python-twitter to create API object.

--- twitter-list-members-move.py
+++ twitter-list-members-move.py
@@ -2,6 +2,7 @@
 
 import configparser
 import os
+import twitter
 
 def work():
     home = os.environ['HOME']
@@ -9,6 +10,12 @@ def work():
 
     c = configparser.ConfigParser()
     c.read(f_conf)
+
+    t_ak = c['default']['twitter_access_token_key']
+    t_as = c['default']['twitter_access_token_secret']
+    t_ck = c['default']['twitter_consumer_key']
+    t_cs = c['default']['twitter_consumer_secret']
+    t = twitter.Api(access_token_key=t_ak, access_token_secret=t_as, consumer_key=t_ck, consumer_secret=t_cs)
 
 if '__main__' == __name__:
     work()
