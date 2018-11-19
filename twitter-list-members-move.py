f6dcd9332b0fc89dccc1f49777b6626510089906
Gea-Suan Lin <gslin@gslin.org>
2018-11-19 11:21:00 +0800

    Add some chars to messages.

--- twitter-list-members-move.py
+++ twitter-list-members-move.py
@@ -28,16 +28,16 @@ def work():
     for i in range(0, cnt):
         try:
             user = users[i]
-            print('Processing {} now...'.format(user.screen_name))
+            print('* Processing {} now...'.format(user.screen_name))
 
-            print('Delete {} from {} now...'.format(user.screen_name, list1))
+            print('- Delete {} from {} now...'.format(user.screen_name, list1))
             t.DestroyListsMember(slug=list1, owner_screen_name=user1, screen_name=user.screen_name)
 
-            print('Add {} to {} now...'.format(user.screen_name, list2))
+            print('+ Add {} to {} now...'.format(user.screen_name, list2))
             t.CreateListsMember(slug=list2, owner_screen_name=user2, screen_name=user.screen_name)
 
         except twitter.error.TwitterError as e:
-            print('Got error {}'.format(e.message))
+            print('! Got error {}'.format(e.message))
 
 if '__main__' == __name__:
     work()
