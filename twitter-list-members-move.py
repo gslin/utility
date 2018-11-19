1228a8060e0e722c3c882164b800323bd877ec85
Gea-Suan Lin <gslin@gslin.org>
2018-11-19 11:19:39 +0800

    Handle error from Twitter.

--- twitter-list-members-move.py
+++ twitter-list-members-move.py
@@ -26,14 +26,18 @@ def work():
 
     users = t.GetListMembers(slug=list1, owner_screen_name=user1)
     for i in range(0, cnt):
-        user = users[i]
-        print('Processing {} now...'.format(user.screen_name))
+        try:
+            user = users[i]
+            print('Processing {} now...'.format(user.screen_name))
 
-        print('Delete {} from {} now...'.format(user.screen_name, list1))
-        t.DestroyListsMember(slug=list1, owner_screen_name=user1, screen_name=user.screen_name)
+            print('Delete {} from {} now...'.format(user.screen_name, list1))
+            t.DestroyListsMember(slug=list1, owner_screen_name=user1, screen_name=user.screen_name)
 
-        print('Add {} to {} now...'.format(user.screen_name, list2))
-        t.CreateListsMember(slug=list2, owner_screen_name=user2, screen_name=user.screen_name)
+            print('Add {} to {} now...'.format(user.screen_name, list2))
+            t.CreateListsMember(slug=list2, owner_screen_name=user2, screen_name=user.screen_name)
+
+        except twitter.error.TwitterError as e:
+            print('Got error {}'.format(e.message))
 
 if '__main__' == __name__:
     work()
