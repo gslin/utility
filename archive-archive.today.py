7df5d4fda87f71cee189de24672423aa839f4273
Gea-Suan Lin <gslin@gslin.org>
2019-03-07 16:04:20 +0800

    archive.today script.

--- archive-archive.today.py
+++ archive-archive.today.py
@@ -19,19 +19,8 @@ def archive_archive_fo(url):
 
     b.close()
 
-def archive_web_archive_org(url):
-    url = 'https://web.archive.org/save/' + url
-    res = requests.get(url)
-
-    print('* Submitted')
-
 def main():
-    os.environ['all_proxy'] = 'socks5://127.0.0.1:9050'
-
     url = sys.argv[1]
-
-#    print('* Calling web.archive.org: {}'.format(url))
-#    archive_web_archive_org(url)
 
     print('* Calling archive.fo: {}'.format(url))
     archive_archive_fo(url)
